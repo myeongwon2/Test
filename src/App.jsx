53df81f85b4b583ce37a44a7170eda615be1f472
Myeon Won <dnfroge@gmail.com>
2022-06-23 02:04:41 +0900

    styled-componets연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Conatainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #bdc3c7;
+`;
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 200px;
+  height: 100px;
+  color: white;
+  font-weight: 600;
+  font-size: 35px;
+  cursor: pointer;
+  &:active,
+  &:foucus {
+    outline: none;
+  }
+  background-color: ${props => (props.danger ? '#e74c3c' : '#2ecc71')};
+`;
 
 function App() {
-  return <div className='App'>안녕하세요</div>;
+  return (
+    <Conatainer>
+      <Button>Hello</Button>
+      <Button danger>Hello</Button>
+    </Conatainer>
+  );
 }
 
 export default App;
